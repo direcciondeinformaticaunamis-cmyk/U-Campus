c1a49f422a702bd1e24f8f007a979960e55c8987
direcciondeinformaticaunamis-cmyk <direcciondeinformaticaunamis@gmail.com>
2026-05-19 08:54:45 -0300

    Delete lib directory

--- lib/config.dart
+++
@@ -1,7 +0,0 @@
-class AppConfig {
-  // Configuración para desarrollo local (Emulator IP para Node.js)
-  static const String apiUrl = 'http://10.0.2.2:3000';
-  
-  // URL de producción (Comentada para pruebas locales)
-  // static const String apiUrl = 'https://apps.unamis.edu.py';
-}
